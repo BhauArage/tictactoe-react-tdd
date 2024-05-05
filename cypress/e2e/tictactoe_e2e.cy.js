0b7025facd69e49bb6719f74d2208767a225bb14
bhavika <bhauarage@gmail.com>
2024-05-05 18:03:04 +0530

    finished e2e testing

--- cypress/e2e/tictactoe_e2e.cy.js
+++ cypress/e2e/tictactoe_e2e.cy.js
@@ -1,15 +1,29 @@
+import Square from "../../src/Square/Square";
+
 describe('Board test', () => {
-    it('plays X O in square', () => {
+    it('Winner declared', () => {
         cy.visit('http://localhost:3000');
-        cy.get('.square').eq(0).click();
-        cy.get('.square').eq(1).click();
-        cy.get('.square').eq(5).click();
-
-    //     cy.get('[data-testid="square1"]')
-    //         .click();
-
-    //     cy.get('[data-testid="square1"]')
-    //         .should('have.value', 'X');
-
+        cy.get('[data-testid="square"]').eq(0).click();
+        cy.get('[data-testid="square"]').eq(3).click();//O
+        cy.get('[data-testid="square"]').eq(1).click();
+        cy.get('[data-testid="square"]').eq(4).click();
+        cy.get('[data-testid="square"]').eq(2).click();
+        cy.get('[data-testid="status"]').contains("Winner: X")
     });
+
+    it("alternates X and O",()=>{
+        cy.visit('http://localhost:3000')
+        cy.get('[data-testid="square"]').eq(0).click();
+        cy.get('[data-testid="square"]').eq(0).contains("X");
+        cy.get('[data-testid="square"]').eq(3).click();//O
+        cy.get('[data-testid="square"]').eq(3).contains("O");
+    });
+
+    it("disables a clicked button",()=>{
+        cy.visit('http://localhost:3000')
+        cy.get('[data-testid="square"]').eq(0).click();
+        cy.get('[data-testid="square"]').eq(0).contains("X");
+        cy.get('[data-testid="square"]').eq(0).click();//O
+        cy.get('[data-testid="square"]').eq(0).contains("X");
+    })
 });
